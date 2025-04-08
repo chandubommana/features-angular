0997491228e434ce395794c6c06b4ffeb49ac883
Chandu <bchandu@ariqt.com>
2025-04-08 11:46:22 +0530

    modified

--- features/src/app/app.component.ts
+++ features/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { PaginatorComponent } from './Paginator/paginator.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PaginatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
